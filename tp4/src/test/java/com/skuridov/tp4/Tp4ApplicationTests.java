c052d64f48d6794ae17f959fb58b1c4deb177fe1
yskuridov <yskuridov@gmail.com>
2022-05-08 22:45:20 -0400

    Project initialized

---
+++ tp4/src/test/java/com/skuridov/tp4/Tp4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skuridov.tp4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
